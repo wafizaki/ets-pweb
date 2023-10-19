461dbd8d89f02d2e1da45a13cb7c4b09da42c7c2
wafizaki <wafizakihanif@gmail.com>
2023-10-20 02:34:46 +0700

    feat:add tugas ets

---
+++ script.js
@@ -0,0 +1,5 @@
+var form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  var username = document.getElementById("username").value;
+  var komen = document.getElementById("komen").value;
+});
